e61566cebc606c1a9bf361629ead27b2be241f34
Ринат Ташев <tashev111@gmail.com>
2026-09-06 09:32:06 +0300

    Подключена проектная таксономия в конструктор

--- catalog-builder-manifest.js
+++ catalog-builder-manifest.js
@@ -11,5 +11,6 @@ window.RK_CATALOG_BUILDER_SCRIPTS = [
   './document-taxonomy-extra-2.js?v=1',
   './document-taxonomy-extra-3.js?v=1',
   './document-taxonomy-extra-4.js?v=1',
-  './document-taxonomy-extra-5.js?v=1'
+  './document-taxonomy-extra-5.js?v=1',
+  './document-taxonomy-extra-6.js?v=1'
 ];
