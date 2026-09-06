9683d9007d4c25769b13afa35a81c6e3f3551bc1
Ринат Ташев <tashev111@gmail.com>
2026-09-06 05:24:27 +0300

    Конструктор подключён к третьему слою реестра документов

--- catalog-builder-manifest.js
+++ catalog-builder-manifest.js
@@ -6,5 +6,6 @@ window.RK_CATALOG_BUILDER_SCRIPTS = [
   './catalog-extra-4.js?v=1',
   './catalog-extra-5.js?v=1',
   './document-taxonomy.js?v=1',
-  './document-taxonomy-extra-2.js?v=1'
+  './document-taxonomy-extra-2.js?v=1',
+  './document-taxonomy-extra-3.js?v=1'
 ];
