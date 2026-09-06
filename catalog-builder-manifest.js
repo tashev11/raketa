b60dca8240ee5020290d9fb6464390447d7710fc
Ринат Ташев <tashev111@gmail.com>
2026-09-06 16:44:50 +0300

    Подключена новая таксономия в конструктор каталога

--- catalog-builder-manifest.js
+++ catalog-builder-manifest.js
@@ -19,5 +19,6 @@ window.RK_CATALOG_BUILDER_SCRIPTS = [
   './document-taxonomy-extra-9.js?v=1',
   './document-taxonomy-extra-10.js?v=1',
   './document-taxonomy-extra-11.js?v=1',
-  './document-taxonomy-extra-12.js?v=1'
+  './document-taxonomy-extra-12.js?v=1',
+  './document-taxonomy-extra-13.js?v=1'
 ];
