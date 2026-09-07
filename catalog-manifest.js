c497139b21e9bf1caf887f71db5b961e4d19da1f
Ринат Ташев <tashev111@gmail.com>
2026-09-07 12:57:33 +0300

    Обновлена версия блока главной для центра сервисов

--- catalog-manifest.js
+++ catalog-manifest.js
@@ -94,5 +94,5 @@ window.RK_EXTENSION_SCRIPTS = [
   './catalog-policy.js?v=2',
   './document-search.js?v=2',
   './guest-access.js?v=3',
-  './home-directions.js?v=4'
+  './home-directions.js?v=5'
 ];
